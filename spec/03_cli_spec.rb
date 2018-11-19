db738ff1a6f2dde3da6748889c84cd64b15eff86
Peter Kreppein <pmkreppein@gmail.com>
2018-11-19 16:53:48 +0000

    automatically backed up by learn

--- spec/03_cli_spec.rb
+++ spec/03_cli_spec.rb
@@ -2,10 +2,24 @@ require_relative "../lib/move.rb"
 
 describe './bin/move executing a CLI Application' do
   it 'defines a board variable' do
+    allow($stdout).to receive(:puts)
+    allow(self).to receive(:gets).and_return("1")
+    allow(self).to receive(:move)
 
+    board = get_variable_from_file("./bin/move", "board")
+
+    expect(board).to eq([" ", " ", " ", " ", " ", " ", " ", " ", " "])
   end
 
-  
+  it 'prints "Welcome to Tic Tac Toe!"' do
+    allow($stdout).to receive(:puts)
+    allow(self).to receive(:gets).and_return("1")
+
+    expect($stdout).to receive(:puts).with("Welcome to Tic Tac Toe!"), "Make sure `bin/move` has code that can output 'Welcome to Tic Tac Toe!' exactly."
+
+    run_file("./bin/move")
+  end
+
   it 'asks the user for input' do
     allow($stdout).to receive(:puts)
 
