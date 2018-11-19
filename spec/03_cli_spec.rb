94f61cac9e9c742f0d958096d7ceb641808c4909
Peter Kreppein <pmkreppein@gmail.com>
2018-11-19 16:53:41 +0000

    automatically backed up by learn

--- spec/03_cli_spec.rb
+++ spec/03_cli_spec.rb
@@ -5,15 +5,7 @@ describe './bin/move executing a CLI Application' do
 
   end
 
-  it 'prints "Welcome to Tic Tac Toe!"' do
-    allow($stdout).to receive(:puts)
-    allow(self).to receive(:gets).and_return("1")
-
-    expect($stdout).to receive(:puts).with("Welcome to Tic Tac Toe!"), "Make sure `bin/move` has code that can output 'Welcome to Tic Tac Toe!' exactly."
-
-    run_file("./bin/move")
-  end
-
+  
   it 'asks the user for input' do
     allow($stdout).to receive(:puts)
 
